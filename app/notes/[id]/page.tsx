b13759cf7349cbc64120c2d007a94c175bdcc530
Jake Leslie <jakeleslie221@gmail.com>
2022-11-10 00:32:07 -0500

    Fireship tutorial

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,25 @@
+import styles from '../Notes.module.css';
+
+async function getNote(noteID: string) {
+	const res = await fetch(
+		`http://127.0.0.1:8090/api/collections/notes/records/${noteID}`,
+		{
+			next: { revalidate: 10 }, // revalidate every 10 seconds
+		}
+	);
+	const data = await res.json();
+	return data;
+}
+export default async function NotePage({ params }: any) {
+	const note = await getNote(params.id);
+	return (
+		<div>
+			<h1>notes/{note.id}</h1>
+			<div className={styles.note}>
+				<h3>{note.title}</h3>
+				<h5>{note.content}</h5>
+				<p>{note.created}</p>
+			</div>
+		</div>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+	return (
+		<div>
+			<h1>Home Page</h1>
+			<h1>Some content</h1>
+		</div>
+	);
+}
